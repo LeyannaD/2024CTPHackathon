7f94912c7e3ec806aeca4935b0a8a2345be9d02a
alanc <alanchen8647@gmail.com>
2024-08-23 19:49:29 -0400

    added club activity

--- js/Nav.js
+++ js/Nav.js
@@ -4,6 +4,7 @@ const CampusEvent = document.getElementById("CAMPUSPAGE");
 const CampusPage = document.getElementById("CampusEvent")
 const SupportPage = document.getElementById("SUPPORT");
 const CalenderPage = document.getElementById("CALENDER");
+const ClubPage = document.getElementById("CLUBACTIVITY");
 
 
 function goToCampus(){
@@ -14,22 +15,33 @@ function goToCampus(){
 function  BackToCuny(){
     CunyCampusPage.classList.remove("hide");
     CampusEvent.classList.add("hide");
+    ShowCampusEvent();
 }
 
 function ShowSupport(){
     CampusPage.classList.add("hide");
     SupportPage.classList.remove("hide");
     CalenderPage.classList.add("hide");
+    ClubPage.classList.add("hide");
 }
 
 function ShowCampusEvent(){
     CampusPage.classList.remove("hide");
     SupportPage.classList.add("hide");
     CalenderPage.classList.add("hide");
+    ClubPage.classList.add("hide");
 }
 
 function ShowMyCalender(){
     CampusPage.classList.add("hide");
     SupportPage.classList.add("hide");
     CalenderPage.classList.remove("hide");
+    ClubPage.classList.add("hide");
+}
+
+function ShowClubActivity(){
+    CampusPage.classList.add("hide");
+    SupportPage.classList.add("hide");
+    CalenderPage.classList.add("hide");
+    ClubPage.classList.remove("hide");
 }
